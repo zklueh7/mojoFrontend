b573ebb5a74ee16dbcbb0682a8a6ddcad517b648
zoey klueh <zklueh@gmail.com>
2024-10-05 14:58:30 -0600

    sign up page responsive styling

--- src/homepage/HomepageAnon.jsx
+++ src/homepage/HomepageAnon.jsx
@@ -18,7 +18,7 @@ function HomepageAnon({ login }) {
         <div>
             <h1 className="heading">MoJo</h1>
             <h6 className="subheading">Welcome MoKan volunteers! </h6>
-            <div className="card-body">
+            <div className="card-body col-md-8 offset-md-2 col-lg-6 offset-lg-3">
                     <LoginForm login={login} />
                     <h2 className="or"><span>OR</span></h2>
                     <Link className="btn"
